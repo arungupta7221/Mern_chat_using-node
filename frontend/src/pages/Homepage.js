cc3747719e833e10d43f338131cd20b5b8d1de45
arungupta7221 <48250441+arungupta7221@users.noreply.github.com>
2023-02-14 23:31:42 +0530

    added login and signUp page on Frontend

--- frontend/src/pages/Homepage.js
+++ frontend/src/pages/Homepage.js
@@ -1,5 +1,7 @@
 import { Box, Container, Tab, TabList, TabPanel, TabPanels, Tabs, Text } from '@chakra-ui/react'
 import React from 'react'
+import Login from '../components/Login'
+import SignUp from '../components/SignUp'
 
 const Homepage = () => {
   return (
@@ -26,10 +28,10 @@ const Homepage = () => {
           </TabList>
           <TabPanels>
             <TabPanel>
-              <p>one!</p>
+              <Login />
             </TabPanel>
             <TabPanel>
-              <p>two!</p>
+              <SignUp />
             </TabPanel>
           </TabPanels>
         </Tabs>
